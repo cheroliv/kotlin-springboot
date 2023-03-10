112dba5a66705cc2ca079b38a695f520f3e5120e
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-03-10 02:52:55 +0100

    Refactor, rename & clean-up code.

--- src/test/kotlin/webapp/password/PasswordServiceTests.kt
+++ src/test/kotlin/webapp/password/PasswordServiceTests.kt
@@ -1,4 +1,30 @@
+@file:Suppress("NonAsciiCharacters")
+
 package webapp.password
 
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.springframework.context.ConfigurableApplicationContext
+import webapp.deleteAllAccounts
+import webapp.launcher
+import kotlin.test.Test
+
 class PasswordServiceTests {
+    private lateinit var context: ConfigurableApplicationContext
+    @BeforeAll
+    fun `lance le server en profile test`() {
+        context = launcher()
+    }
+
+    @AfterAll
+    fun `arrête le serveur`() = context.close()
+
+    @AfterEach
+    fun tearDown() = context.deleteAllAccounts()
+
+    @Test
+    fun `canary`() {
+
+    }
 }
